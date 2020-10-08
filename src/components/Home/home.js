8a004e1005d44c56c3bf4dfcd5c582044e40173d
ypablo <yfedora@gmail.com>
2020-10-08 15:38:13 +0100

    Finalising tools section

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -6,118 +6,122 @@ import { AiOutlineCaretDown, AiFillCaretUp } from 'react-icons/ai';
 
 
 function Home() {
-    const [showLinks, setLinks] = useState(false)
-    const [showFrame, setFrame] = useState(false)
+    const [showLinks1, setLinks1] = useState(false)
+    const [showLinks2, setLinks2] = useState(false)
+    //const onClick = () => setFrame(false)
+    const files = ["bm calls", "battery calculator", "declarations", "STOR calls", "BM planner", "Ops Update Files"];
+    const tools = ["bm", "boa", "horizon", "rebooter", "soc", "wiki", "plc1", "plc2", "stor uuc", "battery monitor"];
+    const [myPro, setMyPro] = useState("");
 
-    const navStyle = {
-        color: "black",
-        textDecoration: "none",
-        padding: "0 25px"
-    }
-
-    function handleClick(e) {
-        //e.stopPropagation();
-        //var x = e.currentTarget;
-        //this.setState({showFrame: !showFrame})
-        console.log(e.currentTarget);
-      }
+    //const navStyle = {
+    //    color: "black",
+    //    textDecoration: "none",
+    //    padding: "0 25px"
+    //}
     
     
     return (
         <div className="home">
            <h1 className="home-title">Home</h1>
-           <h2 className="home-subtitle">Files</h2>
-            <div className="div-outline" onClick={() => setLinks(!showLinks)}>
-                <h2 className="home-subtitle">Tools
-                    {showLinks ? 
+           
+        {/*First link - Files */}
+           <div className="div-outline" onClick={() => setLinks1(!showLinks1)}>
+                <h2 className="home-subtitle">Files
+                    {showLinks1 ? 
                         <button className="button-arrow-down" type="button" ><AiOutlineCaretDown/></button> 
                         :
                         <button className="button-arrow-up" type="button" ><AiFillCaretUp/></button> }
                 </h2>
             </div>
-                {showLinks ?
-                <div> 
-                    {showFrame ? 
-                        <div>
-                            <button type="button" onClick={() => setFrame(!showFrame) } className="button-close">Close</button>
-                            <iframe title="Frame1" className="frames" src={"http://10.0.64.131:10080/unit_unavailability"}/>
-                        </div>
-                        : 
-                        <p className="frame-link" onClick={()=> setFrame(!showFrame)}>
-                            Unit unavailability / Schduling / Forecast management / Unit configuration
-                        </p>
-                    }    
+            {showLinks1 ?
+            <div>
 
-                    {showFrame ? 
-                        <div>
-                            <button type="button" onClick={(e) => handleClick(e, !showFrame) } className="button-close">Close</button>
-                            <iframe title="Frame1" className="frames" src={"http://reports.vm.martinenergy.local/~iainh/boa.py"}/>
-                        </div>
-                        : 
-                        <p className="frame-link" onClick={()=> setFrame(!showFrame)}>
-                            BOA prices
-                        </p>
-                    }    
-                    
+            </div>
+            :
+            null}
 
-
-
-                    <p>
-                        <a style={navStyle} onClick={(e) => handleClick(e)}
-                        href="http://reports.vm.martinenergy.local/~iainh/boa.py" 
-                        target="_blank" rel="noopener noreferrer">BOA prices</a>
-                    </p>
-                    <p>
-                        <a style={navStyle} 
-                        href="https://www.unlimitedhorizon.co.uk/webapp/signin" 
-                        target="_blank" rel="noopener noreferrer">Horizon</a>
-                    </p>
-                    <p>
-                        <a style={navStyle} 
-                        href="http://192.168.16.79/cgi-bin/gsm.pl" 
-                        target="_blank" rel="noopener noreferrer">Rebooter</a>
-                    </p>
-                    <p>
-                        <a style={navStyle} 
-                        href="http://reports.vm.martinenergy.local/~iainh/containers.py" 
-                        target="_blank" rel="noopener noreferrer">SoC</a>
-                    </p>
-                    <p>
-                        <a style={navStyle} 
-                        href="http://wiki.vm.martinenergy.local/mediawiki/index.php/Wikitricity_Home" 
-                        target="_blank" rel="noopener noreferrer">Wiki</a>
-                    </p>
-                    <p>
-                        <a style={navStyle} 
-                        href="http://reports.vm.martinenergy.local/~iainh/raw_plc.py" 
-                        target="_blank" rel="noopener noreferrer">PLC schedule 1</a>
-                    </p>
-                    <p>
-                        <a style={navStyle} 
-                        href="http://reports.vm.martinenergy.local/~iainh/plc.py" 
-                        target="_blank" rel="noopener noreferrer">PLC schedule 2</a>
-                        </p>
-                    <p>
-                        <a style={navStyle} 
-                        href="https://10.0.64.23/gp/uuc/" 
-                        target="_blank" rel="noopener noreferrer">STOR UUC</a>
-                    </p>
-
-
-                    {showFrame ? 
-                        <div>
-                            <button type="button" onClick={() => setFrame(!showFrame) } className="button-close">Close</button>
-                            <iframe title="Frame10" className="frames" src={"http://192.168.16.74:8888/"}/>
-                        </div>
-                        :           
-                        <p className="frame-link" onClick={()=> setFrame(!showFrame)}>
-                            Battery monitor
-                        </p>
-                    }
-                    
-                </div> 
-                : 
-                null }
+        {/*Second link - Tools */}
+            <div className="div-outline" onClick={() => setLinks2(!showLinks2)}>
+                <h2 className="home-subtitle">Tools
+                    {showLinks2 ? 
+                        <button className="button-arrow-down" type="button" ><AiOutlineCaretDown/></button> 
+                        :
+                        <button className="button-arrow-up" type="button" ><AiFillCaretUp/></button> }
+                </h2>
+            </div>
+            {showLinks2 ?
+            <div>  
+                {tools.map(tool => (
+                    <button
+                    key={tool}
+                    className="buttons-submenu"
+                    onClick={() => setMyPro(tool)}>
+                        {tool.toLocaleUpperCase()}
+                    </button>
+                    ))}
+                    {myPro === "bm" && (
+                        <>
+                        <button type="button" onClick={() => setMyPro(null) } className="button-close">Close</button>
+                        <iframe title="Frame1" className="frames" src={"http://10.0.64.131:10080/unit_unavailability"}/>
+                        </>
+                    )}
+                    {myPro === "boa" && (
+                        <>
+                        <button type="button" onClick={() => setMyPro(null) } className="button-close">Close</button>
+                        <iframe title="Frame2" className="frames" src={"http://reports.vm.martinenergy.local/~iainh/boa.py"}/>
+                        </>
+                    )}
+                    {myPro === "horizon" && (
+                        <>
+                        <button type="button" onClick={() => setMyPro(null) } className="button-close">Close</button>
+                        <iframe title="Frame3" className="frames" src={"https://www.unlimitedhorizon.co.uk/webapp/signin"}/>
+                        </>
+                    )}
+                    {myPro === "rebooter" && (
+                        <>
+                        <button type="button" onClick={() => setMyPro(null) } className="button-close">Close</button>
+                        <iframe title="Frame4" className="frames" src={"http://192.168.16.79/cgi-bin/gsm.pl"}/>
+                        </>
+                    )}
+                    {myPro === "soc" && (
+                        <>
+                        <button type="button" onClick={() => setMyPro(null) } className="button-close">Close</button>
+                        <iframe title="Frame5" className="frames" src={"http://reports.vm.martinenergy.local/~iainh/containers.py"}/>
+                        </>
+                    )}
+                    {myPro === "wiki" && (
+                        <>
+                        <button type="button" onClick={() => setMyPro(null) } className="button-close">Close</button>
+                        <iframe title="Frame6" className="frames" src={"http://wiki.vm.martinenergy.local/mediawiki/index.php/Wikitricity_Home"}/>
+                        </>
+                    )}
+                    {myPro === "plc1" && (
+                        <>
+                        <button type="button" onClick={() => setMyPro(null) } className="button-close">Close</button>
+                        <iframe title="Frame7" className="frames" src={"http://reports.vm.martinenergy.local/~iainh/raw_plc.py"}/>
+                        </>
+                    )}
+                    {myPro === "plc2" && (
+                        <>
+                        <button type="button" onClick={() => setMyPro(null) } className="button-close">Close</button>
+                        <iframe title="Frame8" className="frames" src={"http://reports.vm.martinenergy.local/~iainh/plc.py"}/>
+                        </>
+                    )}
+                    {myPro === "stor uuc" && (
+                        <>
+                        <button type="button" onClick={() => setMyPro(null) } className="button-close">Close</button>
+                        <iframe title="Frame9" className="frames" src={"https://10.0.64.23/gp/uuc/"}/>
+                        </>
+                    )}
+                    {myPro === "battery monitor" && (
+                        <>
+                        <button type="button" onClick={() => setMyPro(null) } className="button-close">Close</button>
+                        <iframe title="Frame10" className="frames" src={"http://192.168.16.74:8888/"}/>
+                        </>
+                    )}            
+            </div>
+            : 
+            null }
            <h2 className="home-subtitle">Contacts</h2>
            <h2 className="home-subtitle">Docs</h2>
 
